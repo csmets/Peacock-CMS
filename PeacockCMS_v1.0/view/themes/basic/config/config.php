fb149674377026c3bd5835b74c73c6bcc92eb2b0
Clyde Smets <clyde.smets@gmail.com>
2015-06-15 23:04:23 +0900

    no message

--- PeacockCMS_v1.0/view/themes/basic/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $enableCSSEditor = true;
-    define("CSS_EDITOR_FILE","css/basicPageStyle.css");
-
-?>
--- PeacockCMS_v1.0/view/themes/basic/includes/footer.php
+++
@@ -1,4 +0,0 @@
-
-<div class='poweredPeaockWrapper'>
-    <span class='poweredPeaock'><a href='http://www.peacockcms.com' target='_blank'>powered by Peacock cms</a></span>
-</div>
